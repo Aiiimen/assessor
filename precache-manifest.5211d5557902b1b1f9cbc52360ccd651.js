afd60006df6efa65fde692c6ffbb4c5706079b14
Aimen <aimenbenfika@gmail.com>
2019-06-30 15:26:23 +0100

    Updates

---
+++ precache-manifest.5211d5557902b1b1f9cbc52360ccd651.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9e20d47926d0104fcc930c356b47c907",
+    "url": "/assessor/index.html"
+  },
+  {
+    "revision": "1e17757a96415e540b25",
+    "url": "/assessor/static/css/main.5ba46a3e.chunk.css"
+  },
+  {
+    "revision": "9eee67cf782a38d35aeb",
+    "url": "/assessor/static/js/2.d6a534e5.chunk.js"
+  },
+  {
+    "revision": "1e17757a96415e540b25",
+    "url": "/assessor/static/js/main.5abef9b7.chunk.js"
+  },
+  {
+    "revision": "aac42d07f20bc8375863",
+    "url": "/assessor/static/js/runtime~main.82b7eea2.js"
+  }
+]);
